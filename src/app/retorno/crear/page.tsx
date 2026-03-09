157322d59bb2dc72dbccab6dcd2922f8f6995608
LinaDiaz <linaalexdiaz@unicauca.edu.co>
2026-03-09 15:52:54 -0500

    Creación de layout
    Se realiza la creación del layout y se envuelve el componente

--- src/app/retorno/crear/page.tsx
+++ src/app/retorno/crear/page.tsx
@@ -1,10 +1,4 @@
 import CrearRetorno from "@/features/gestion/retorno/components/CrearRetorno";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-    title: "Crear Retorno | Caravana del Retorno",
-    description: "Crear un nuevo retorno en la plataforma Caravana del Retorno",
-};
 
 export default function CrearRetornoPage() {
     return <CrearRetorno />;
