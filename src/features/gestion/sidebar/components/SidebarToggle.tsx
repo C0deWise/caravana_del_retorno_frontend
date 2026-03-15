27434729b88b02902bee0edf1f4dfc91b74d621e
Juan Manuel Ante <juan09ante@gmail.com>
2026-03-15 01:57:40 -0500

    deleted unnecesary comment

--- src/features/gestion/sidebar/components/SidebarToggle.tsx
+++ src/features/gestion/sidebar/components/SidebarToggle.tsx
@@ -9,16 +9,15 @@ export default function SidebarToggle() {
   return (
     <button
       onClick={toggleCollapse}
-      className="flex items-center p-4 pl-8 border-b-2 border-secondary rounded-xl justify-start text-primary hover:bg-gray-100 rounded-t-xl transition-colors" // ← justify-start
+      className="flex items-center p-4 pl-8 border-b-2 border-secondary rounded-xl justify-start text-primary hover:bg-gray-100 rounded-t-xl transition-colors"
     >
       <ListBulletIcon className="w-6 h-6 shrink-0 transition-colors" />
       <span
         className={`
           truncate text-xl transition-all duration-300 ease-in-out ml-4
-          ${
-            isCollapsed
-              ? "opacity-0 w-0 scale-x-50 -translate-x-2"
-              : "opacity-100 w-full scale-x-100 translate-x-0"
+          ${isCollapsed
+            ? "opacity-0 w-0 scale-x-50 -translate-x-2"
+            : "opacity-100 w-full scale-x-100 translate-x-0"
           }
         `}
       >
