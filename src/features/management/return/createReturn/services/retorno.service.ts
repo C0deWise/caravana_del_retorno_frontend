64a918a8928def6a8dd0e48974c841dd613b7e75
LinaDiaz <linaalexdiaz@unicauca.edu.co>
2026-04-17 09:32:42 -0500

    fix: corrección de enpoint de retorno
    Se corrige el endpoint de retorno

--- src/features/management/return/createReturn/services/retorno.service.ts
+++ src/features/management/return/createReturn/services/retorno.service.ts
@@ -2,7 +2,7 @@ import { apiService } from "@/services/api.services";
 import type { Retorno, RetornoCreateRequest } from "../../types/retorno.types";
 
 class RetornoService {
-  private endpoint = "/retornos/";
+  private endpoint = "/api/v1/retornos/";
 
   async create(data: RetornoCreateRequest): Promise<Retorno> {
     return apiService.post<Retorno>(this.endpoint, data);
