8980e5cc9d663a438f08daaace45f215a543faef
LinaDiaz <linaalexdiaz@unicauca.edu.co>
2026-04-18 16:05:31 -0500

    feat: Formulario para darse de baja de un retorno

---
+++ src/features/management/return/returnUnregistration/hook/useSendReturnUnregistration.hook.ts
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { returnUnregistrationService } from  "../service/returnUnregistration.service";
+import type { ReturnUnregistrationApi, ReturnUnregistrationCreateRequest } from "../types/returnUnregistration.types";
+
+interface UseSendReturnUnregistration {
+    sendReturnUnregistration:(data: ReturnUnregistrationCreateRequest) => Promise<ReturnUnregistrationApi | null>;
+    loading: boolean;
+    error: string | null;
+    success: boolean;
+}
+
+export const useSendReturnUnregistration = (): UseSendReturnUnregistration => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    const [success, setSuccess] = useState(false);
+
+    const sendReturnUnregistration = async (data: ReturnUnregistrationCreateRequest): Promise<ReturnUnregistrationApi | null> => {
+        setLoading(true);
+        setError(null);
+        setSuccess(false);
+        try {
+            const response = await returnUnregistrationService.createReturnRegistration(data);
+            setSuccess(true);
+            return response;
+        }
+        catch (error) {
+            const mensaje =
+                error instanceof Error ? error.message : "Error al enviar la desinscripción de retorno";
+            setError(mensaje);
+            return null;
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return {
+        sendReturnUnregistration,
+        loading,
+        error,
+        success,
+    };
+}
