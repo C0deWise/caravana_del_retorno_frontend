ab172c67931f2de6147e7e035f85a679bb16e22e
Lina Diaz <168787968+LinaAlexandra2812@users.noreply.github.com>
2026-05-10 23:49:12 -0500

    feat: navegación a través del tipo de inscripción [skip ci]

--- src/app/gestion/retorno/registro/individual/page.tsx
+++ src/app/gestion/retorno/registro/individual/page.tsx
@@ -1,5 +1,5 @@
 import ReturnRegistrationForm from "@/features/management/return/returnRegistrationForm/components/ReturnRegistrationForm";
 
-export default function RegistroIndividualPage() {
+export default function Retorno() {
   return <ReturnRegistrationForm />;
 }
